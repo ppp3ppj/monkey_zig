5d70e456900f96a25948df1c41afd814fa3ad54d
ppp3ppj <ppp.3ppj@gmail.com>
2024-09-15 15:47:06 +0700

    change struct to use tag union

--- src/token/token.zig
+++ src/token/token.zig
@@ -1,27 +1,19 @@
 const std = @import("std");
 
-pub const Token = struct {
-    Type: Tag,
-    Literal: [:0]const u8,
+pub const Token = union(enum) {
+    ident: []const u8,
+    int: []const u8,
+
+    illegal,
+    eof,
+    assign,
+    plus,
+    comma,
+    semicolon,
+    lparen,
+    rparen,
+    lsquirly,
+    rsquirly,
+    function,
+    let,
 };
-
-pub const Tag = enum { illegal, eof, ident, int, assign, plus, comma, semicolon, lparen, rparen, lbrace, rbrace, function, let };
-
-pub fn lexeme(tag: Tag) ?[]const u8 {
-    return switch(tag) {
-        .illegal => "ILLEGAL",
-        .eof => "EOF",
-        .ident => "IDENT",
-        .int => "INT",
-        .assign => "=",
-        .plus => "+",
-        .comma => ",",
-        .semicolon => ";",
-        .lparen => "(",
-        .rparen => ")",
-        .lbrace => "{",
-        .rbrace => "}",
-        .function => "FUNCTION",
-        .let => "LET",
-    };
-}
