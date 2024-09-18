19469f6c8854a2993b6843911ce88c75c909213f
ppp3ppj <ppp.3ppj@gmail.com>
2024-09-18 21:08:38 +0700

    add full test lexer

--- test/tests.zig
+++ test/tests.zig
@@ -30,3 +30,62 @@ test "Lexer" {
     //     std.log.warn("{s}", .{f.name});
     // }
 }
+
+test "Lexer - Full" {
+    const input =
+        \\let five = 5;
+        \\let ten = 10;
+        \\let add = fn(x, y) {
+        \\  x + y;
+        \\};
+        \\let result = add(five, ten);
+    ;
+
+    var lex = lexer.Lexer.init(input);
+    const tokens = [_]token.Token{
+        .let,
+        .{.indent = "five"},
+        .assign,
+        .{.int = "5"},
+        .semicolon,
+        .let,
+        .{.indent = "ten"},
+        .assign,
+        .{.int = "10"},
+        .semicolon,
+        .let,
+        .{.indent = "add"},
+        .assign,
+        .function,
+        .lparen,
+        .{.indent = "x"},
+        .comma,
+        .{.indent = "y"},
+        .rparen,
+        .lsquirly,
+        .{.indent = "x"},
+        .plus,
+        .{.indent = "y"},
+        .semicolon,
+        .rsquirly,
+        .semicolon,
+        .let,
+        .{.indent = "result"},
+        .assign,
+        .{.int = "add"},
+        .lparen,
+        .{.indent = "five"},
+        .comma,
+        .{.indent = "ten"},
+        .rparen,
+        .semicolon,
+
+        .eof,
+    };
+
+    for (tokens) |t| {
+        const tok = lex.next_token();
+
+        try expectEqualDeep(t, tok);
+    }
+}
