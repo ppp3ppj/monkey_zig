6ca1db4e735383e1f819ef44fd25ec640fda4352
ppp3ppj <ppp.3ppj@gmail.com>
2024-09-19 20:29:56 +0700

    add test at main

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,22 @@
 const std = @import("std");
 const lexer = @import("lexer");
+const token = @import("token");
 
 pub fn main() !void {
-    const l = lexer.Lexer.init("+-");
-    std.debug.print("{s}", .{l.input});
+
+    var lex = lexer.Lexer.init("let five = 5;");
+    const tokens = [_]token.Token{
+        .let,
+        .{.ident = "five"},
+        .assign,
+        .{.int = "5"},
+        .semicolon,
+
+        .eof,
+    };
+
+    for (tokens) |t| {
+        const tok = lex.next_token();
+        std.debug.print("{s} at token: {s}", .{tok, t});
+    }
 }
