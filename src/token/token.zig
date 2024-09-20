be2cffdbf6a7301a050b8f1a40f345bdb2bd6acc
ppp3ppj <ppp.3ppj@gmail.com>
2024-09-20 21:31:08 +0700

    add equal token

--- src/token/token.zig
+++ src/token/token.zig
@@ -15,15 +15,19 @@ pub const Token = union(enum) {
     lsquirly,
     rsquirly,
 
+    equal,
+
     let,
     function,
     if_token,
+    return_token,
 
     pub fn keyword(ident: []const u8) ?Token {
         const map = std.StaticStringMap(Token).initComptime(.{
         .{ "let", .let },
         .{ "fn", .function },
         .{ "if", .if_token },
+        .{ "return", .return_token },
         });
 
         return map.get(ident);
